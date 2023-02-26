d1c09f75f40dc700b77973ec18ad8a09521495ee
vellprolol <vellprolol@gmail.com>
2023-02-26 12:20:52 +0300

    Initial commit

---
+++ src/main/java/ru/alexeykuznetsov/spbgti/rbtreeapplication/RbTreeApplication.java
@@ -0,0 +1,13 @@
+package ru.alexeykuznetsov.spbgti.rbtreeapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbTreeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RbTreeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/alexeykuznetsov/spbgti/rbtreeapplication/RbTreeApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.alexeykuznetsov.spbgti.rbtreeapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RbTreeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
